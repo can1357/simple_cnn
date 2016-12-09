63de844a4932d4d1c258d7e5802a0a5324d3dca4
miku <mikyuuhatsune@gmail.com>
2016-12-09 18:26:52 +0300

    added weight decay

--- CNN/optimization_method.h
+++ CNN/optimization_method.h
@@ -2,12 +2,14 @@
 #include "gradient_t.h"
 
 #define LEARNING_RATE 0.01
-#define MOMENTUM 0.4
+#define MOMENTUM 0.6
+#define WEIGHT_DECAY 0.001
 
 static float update_weight( float w, gradient_t& grad, float multp = 1 )
 {
 	float m = (grad.grad + grad.oldgrad * MOMENTUM);
-	w -= LEARNING_RATE  * m * multp;
+	w -= LEARNING_RATE  * m * multp +
+		 LEARNING_RATE * WEIGHT_DECAY * w;
 	return w;
 }
 
